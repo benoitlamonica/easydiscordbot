2102835860143ff0442bd8d5ed29a2ca6564c5e7
Benoit Lamonica <ben33127@gmail.com>
2021-07-09 11:12:58 +0200

    FIX : Discord token

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { EasyDiscordBot } = require('./vendor/config/bot');
 const app = Express();
 
 const PORT = process.env.PORT;
-const TOKEN = process.env.TOKEN;
+const TOKEN = process.env.DISCORD_TOKEN;
 const CMD_PREFIX = process.env.COMMAND_PREFIX;
 
 app.listen(PORT, () => {
